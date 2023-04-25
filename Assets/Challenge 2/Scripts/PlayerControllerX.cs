e15ae12fb492628f8fa6727a55012e949dfacafc
Jon Higgins <jon@jonhiggins.co.uk>
2023-04-25 15:13:01 +0100

    Fix bugs with prototype 2

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private float allowRespawnDelay = 1;
+    private float allowRespawnTimer = 0;
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(allowRespawnTimer);
+        if (allowRespawnTimer > 0)
+        {
+            allowRespawnTimer -= Time.deltaTime;
+        }
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && allowRespawnTimer <= 0)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            allowRespawnTimer = allowRespawnDelay;
         }
     }
+
 }
